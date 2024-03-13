9d3f8be4301ea7e6287c3678660c8e2171760591
Anatoli Zdanevich <anatoli.zdanevitch@gmail.com>
2024-03-13 10:06:11 +0300

    feat: add some resolves

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -192,8 +192,11 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  const newObj = Object.create(proto);
+  Object.assign(newObj, obj);
+  return newObj;
 }
 
 /**
